792b9c89f84f490898937325d8081713b1dc431b
fholee <fholee@gmail.com>
2015-10-18 15:20:46 +0800

    nightly commit

---
+++ custom.el
@@ -0,0 +1,42 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(TeX-engine (quote xetex))
+ '(custom-enabled-themes (quote (tangotango)))
+ '(custom-safe-themes
+   (quote
+    ("5999e12c8070b9090a2a1bbcd02ec28906e150bb2cdce5ace4f965c76cf30476" "49e5a7955b853f70d1fe751b2f896921398b273aa62f47bda961a45f80219581" default)))
+ '(desktop-restore-frames nil)
+ '(diff-command "diff")
+ '(diff-switches "-Nau1")
+ '(ess-ask-for-ess-directory nil)
+ '(ess-keep-dump-files nil)
+ '(ess-swv-pdflatex-commands (quote ("xelatex")))
+ '(ess-swv-processor (quote knitr))
+ '(gud-pdb-command-name "python -m pdb")
+ '(multi-term-program "/bin/zsh")
+ '(org-confirm-babel-evaluate nil)
+ '(org-taskjuggler-process-command "tj3 --no-color --output-dir %o %f")
+ '(org-taskjuggler-reports-directory "output")
+ '(python-shell-interpreter-args "-i")
+ '(python-shell-interpreter-interactive-arg "-i")
+ '(safe-local-variable-values
+   (quote
+    ((no-byte-compile t)
+     (ruby-compilation-executable . "ruby")
+     (ruby-compilation-executable . "ruby1.8")
+     (ruby-compilation-executable . "ruby1.9")
+     (ruby-compilation-executable . "rbx")
+     (ruby-compilation-executable . "jruby"))))
+ '(session-use-package t nil (session))
+ '(showtip-timeout 60)
+ '(sql-postgres-options (quote ("-P" "pager=off")))
+ '(suggest-key-bindings 3))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-link ((t (:underline t)))))
